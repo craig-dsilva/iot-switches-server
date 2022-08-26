7240e064638963b15d0ccbc9daf1dc73f4f84a21
craig-dsilva <craig.s.dsilva@gmail.com>
2022-08-26 11:58:07 +0100

    gpio works

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("Hello World");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const Gpio = require('onoff').Gpio;
+// const io = ioserver(http);
+const s1 = new Gpio(2, 'out');
+http.listen(8080);
+io.on('connection', (socket) => {
+    let s1Val = 0;
+    socket.on('value', (data) => {
+        s1Val = data;
+    });
+});
--- server.ts
+++ server.ts
@@ -1,1 +1,21 @@
-console.log("Hello World")
+import express from 'express';
+// import {Server} from 'http';
+import ioserver, {Socket} from 'socket.io'
+
+const app = express();
+const http = require('http').Server(app)
+const io = require('socket.io')(http);
+const Gpio = require('onoff').Gpio;
+
+// const io = ioserver(http);
+const s1 = new Gpio(2, 'out');
+
+http.listen(8080)
+
+io.on('connection', (socket: Socket) => {
+    let s1Val = 0;
+    socket.on('value', (data) => {
+        s1Val = data;
+        
+    })
+})
