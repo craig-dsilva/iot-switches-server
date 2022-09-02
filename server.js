c6f0ba95529792989d4fe5d064001abcdaa5d928
craig-dsilva <craig.s.dsilva@gmail.com>
2022-09-02 12:15:26 +0100

    It works

--- server.js
+++ server.js
@@ -4,16 +4,52 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const rpi_gpio_1 = __importDefault(require("rpi-gpio"));
+// import path from "path";
+const socket_io_1 = require("socket.io");
+const http_1 = require("http");
 const app = (0, express_1.default)();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const Gpio = require('onoff').Gpio;
-// const io = ioserver(http);
-const s1 = new Gpio(2, 'out');
-http.listen(8080);
+const server = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+    },
+});
+// app.use(express.static(path.join(__dirname, '../client/build')));
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
+server.listen(8000);
+// Variables for the gpio pins
+const INI1 = 8;
+const INI2 = 10;
+const INI3 = 12;
+const INI4 = 16;
+// Gpio modes are set to out and it's state is off
+// P.S. I am setting all to high as my relay board has inverted logic
+rpi_gpio_1.default.setup(INI1, rpi_gpio_1.default.DIR_HIGH);
+rpi_gpio_1.default.setup(INI2, rpi_gpio_1.default.DIR_HIGH);
+rpi_gpio_1.default.setup(INI3, rpi_gpio_1.default.DIR_HIGH);
+rpi_gpio_1.default.setup(INI4, rpi_gpio_1.default.DIR_HIGH);
+// Main switch control
 io.on('connection', (socket) => {
-    let s1Val = 0;
-    socket.on('value', (data) => {
-        s1Val = data;
+    socket.on('s1', (data) => {
+        rpi_gpio_1.default.write(INI1, data);
+    });
+});
+io.on('connection', (socket) => {
+    socket.on('s2', (data) => {
+        rpi_gpio_1.default.write(INI2, data);
+    });
+});
+io.on('connection', (socket) => {
+    socket.on('s3', (data) => {
+        rpi_gpio_1.default.write(INI3, data);
+    });
+});
+io.on('connection', (socket) => {
+    socket.on('s4', (data) => {
+        rpi_gpio_1.default.write(INI4, data);
     });
 });
--- server.ts
+++ server.ts
@@ -1,21 +1,60 @@
 import express from 'express';
-// import {Server} from 'http';
-import ioserver, {Socket} from 'socket.io'
-
+import GPIO from 'rpi-gpio';
+// import path from "path";
+import { Server, Socket } from 'socket.io';
+import { createServer } from 'http';
 const app = express();
-const http = require('http').Server(app)
-const io = require('socket.io')(http);
-const Gpio = require('onoff').Gpio;
+const server = createServer(app);
 
-// const io = ioserver(http);
-const s1 = new Gpio(2, 'out');
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 
-http.listen(8080)
+// app.use(express.static(path.join(__dirname, '../client/build')));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
+
+server.listen(8000);
+
+// Variables for the gpio pins
+const INI1 = 8;
+const INI2 = 10;
+const INI3 = 12;
+const INI4 = 16;
+
+// Gpio modes are set to out and it's state is off
+// P.S. I am setting all to high as my relay board has inverted logic
+GPIO.setup(INI1, GPIO.DIR_HIGH);
+GPIO.setup(INI2, GPIO.DIR_HIGH);
+GPIO.setup(INI3, GPIO.DIR_HIGH);
+GPIO.setup(INI4, GPIO.DIR_HIGH);
+
+// Main switch control
+io.on('connection', (socket: Socket) => {
+  socket.on('s1', (data) => {
+    GPIO.write(INI1, data);
+  });
+});
 
 io.on('connection', (socket: Socket) => {
-    let s1Val = 0;
-    socket.on('value', (data) => {
-        s1Val = data;
-        
-    })
-})
+  socket.on('s2', (data) => {
+    GPIO.write(INI2, data);
+  });
+});
+
+io.on('connection', (socket: Socket) => {
+  socket.on('s3', (data) => {
+    GPIO.write(INI3, data);
+  });
+});
+
+io.on('connection', (socket: Socket) => {
+  socket.on('s4', (data) => {
+    GPIO.write(INI4, data);
+  });
+});
