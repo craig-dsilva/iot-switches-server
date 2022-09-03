6082562b0e7efe9ff1638062c54611a2d3d6dd17
craig-dsilva <craig.s.dsilva@gmail.com>
2022-09-03 14:21:58 +0100

    server now dynamic

--- server.js
+++ server.js
@@ -5,27 +5,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const rpi_gpio_1 = __importDefault(require("rpi-gpio"));
-// import path from "path";
+const path_1 = __importDefault(require("path"));
 const socket_io_1 = require("socket.io");
 const http_1 = require("http");
 const app = (0, express_1.default)();
 const server = (0, http_1.createServer)(app);
-const io = new socket_io_1.Server(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST'],
-    },
+const io = new socket_io_1.Server(server);
+app.use(express_1.default.static(path_1.default.join(__dirname, '../client/build')));
+app.get('/', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '../client/build', 'index.html'));
 });
-// app.use(express.static(path.join(__dirname, '../client/build')));
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-// });
-server.listen(8000);
 // Variables for the gpio pins
 const INI1 = 8;
 const INI2 = 10;
 const INI3 = 12;
 const INI4 = 16;
+// Data of the switches
+const relayState = [
+    { id: 0, switchNumber: 1, GPIO_PIN: INI1, switchState: true },
+    { id: 1, switchNumber: 2, GPIO_PIN: INI2, switchState: true },
+    { id: 2, switchNumber: 3, GPIO_PIN: INI3, switchState: true },
+    { id: 3, switchNumber: 4, GPIO_PIN: INI4, switchState: true }
+];
 // Gpio modes are set to out and it's state is off
 // P.S. I am setting all to high as my relay board has inverted logic
 rpi_gpio_1.default.setup(INI1, rpi_gpio_1.default.DIR_HIGH);
@@ -34,22 +35,12 @@ rpi_gpio_1.default.setup(INI3, rpi_gpio_1.default.DIR_HIGH);
 rpi_gpio_1.default.setup(INI4, rpi_gpio_1.default.DIR_HIGH);
 // Main switch control
 io.on('connection', (socket) => {
-    socket.on('s1', (data) => {
-        rpi_gpio_1.default.write(INI1, data);
-    });
+    socket.emit('state', relayState);
+    for (let i = 0; i < relayState.length; i++) {
+        socket.on(relayState[i].id.toString(), (data) => {
+            relayState[i].switchState = data;
+            rpi_gpio_1.default.write(relayState[i].GPIO_PIN, relayState[i].switchState);
+        });
+    }
 });
-io.on('connection', (socket) => {
-    socket.on('s2', (data) => {
-        rpi_gpio_1.default.write(INI2, data);
-    });
-});
-io.on('connection', (socket) => {
-    socket.on('s3', (data) => {
-        rpi_gpio_1.default.write(INI3, data);
-    });
-});
-io.on('connection', (socket) => {
-    socket.on('s4', (data) => {
-        rpi_gpio_1.default.write(INI4, data);
-    });
-});
+server.listen(8000);
--- server.ts
+++ server.ts
@@ -1,31 +1,32 @@
 import express from 'express';
 import GPIO from 'rpi-gpio';
-// import path from "path";
+import path from "path";
 import { Server, Socket } from 'socket.io';
 import { createServer } from 'http';
+
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
-const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  },
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
-
-// app.use(express.static(path.join(__dirname, '../client/build')));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-// });
-
-server.listen(8000);
 
 // Variables for the gpio pins
 const INI1 = 8;
 const INI2 = 10;
 const INI3 = 12;
 const INI4 = 16;
+
+// Data of the switches
+const relayState = [
+  { id: 0, switchNumber: 1, GPIO_PIN: INI1, switchState: true },
+  { id: 1, switchNumber: 2, GPIO_PIN: INI2, switchState: true },
+  { id: 2, switchNumber: 3, GPIO_PIN: INI3, switchState: true },
+  { id: 3, switchNumber: 4, GPIO_PIN: INI4, switchState: true }
+]
 
 // Gpio modes are set to out and it's state is off
 // P.S. I am setting all to high as my relay board has inverted logic
@@ -36,25 +37,14 @@ GPIO.setup(INI4, GPIO.DIR_HIGH);
 
 // Main switch control
 io.on('connection', (socket: Socket) => {
-  socket.on('s1', (data) => {
-    GPIO.write(INI1, data);
-  });
+  socket.emit('state', relayState)
+
+  for (let i = 0; i < relayState.length; i++) {
+    socket.on(relayState[i].id.toString(), (data) => {
+      relayState[i].switchState = data;
+      GPIO.write(relayState[i].GPIO_PIN, relayState[i].switchState);
+    });
+  }
 });
 
-io.on('connection', (socket: Socket) => {
-  socket.on('s2', (data) => {
-    GPIO.write(INI2, data);
-  });
-});
-
-io.on('connection', (socket: Socket) => {
-  socket.on('s3', (data) => {
-    GPIO.write(INI3, data);
-  });
-});
-
-io.on('connection', (socket: Socket) => {
-  socket.on('s4', (data) => {
-    GPIO.write(INI4, data);
-  });
-});
+server.listen(8000);
